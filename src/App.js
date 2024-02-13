965930bff675824196942a3427b3e6f076a2a599
kimjiyun <faren02134@gmail.com>
2024-02-13 20:04:14 +0900

    사비

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import DarkOrLight from "./DarkOrLight";
+const ThemeContext = React.createContext();
+const UserContext = React.createContext();
+
 
 function App() {
+  return <DarkOrLight />
+}
+
+function A() { return <B />; }
+function B() { return <C />; }
+function C()
+{
+  const color = useContext(ThemeContext);
+  const user = useContext(UserContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>{user.name}</h3>
+      <h1 style={{ backgroundColor: color }}>C 내용</h1>
     </div>
   );
 }
-
 export default App;
